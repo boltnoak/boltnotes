951bf0c7d79d5ba203fafff756fc11c591a62d8b
boltnoak <boltnoak@gmail.com>
2026-09-02 12:49:20 -0300

    patch release

--- src-electron/main.js
+++ src-electron/main.js
@@ -762,8 +762,9 @@ ipcMain.on('drag-window', (event, { mouseX, mouseY }) => {
       assetsReady = true;
       if (assetsWin && !assetsWin.isDestroyed()) {
         assetsWin.webContents.send('assets-ready');
-        assetsWin.destroy();
+        // assetsWin.destroy();
       }
+      assetsWin.hide();
       createWindow();
       preloadAll();
     } catch (err) {
