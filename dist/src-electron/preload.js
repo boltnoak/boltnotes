fdbda74c4ca028ba0a05638a2a5fc643c3ff1edb
boltnoak <boltnoak@gmail.com>
2026-09-17 19:58:30 -0300

    Initial gh-pages branch

---
+++ dist/src-electron/preload.js
@@ -0,0 +1,108 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { contextBridge, ipcRenderer } = require('electron');
+window.addEventListener('DOMContentLoaded', () => {
+    try {
+        const isMaximized = ipcRenderer.sendSync('menu:is-maximized-sync');
+        if (isMaximized) {
+            document.documentElement.classList.add('window-maximized');
+            document.documentElement.classList.remove('window-normal');
+            window.sessionStorage.setItem('windowState', 'maximized');
+        }
+        else {
+            document.documentElement.classList.add('window-normal');
+            document.documentElement.classList.remove('window-maximized');
+            window.sessionStorage.setItem('windowState', 'normal');
+        }
+    }
+    catch (err) {
+        document.documentElement.classList.add('window-normal');
+        window.sessionStorage.setItem('windowState', 'normal');
+    }
+});
+const isDev = process.argv.includes('--development');
+contextBridge.exposeInMainWorld('electronAPI', {
+    onWindowStateChange: (callback) => ipcRenderer.on('window-state-change', (event, state) => callback(state)),
+    existsAssets: (filePath) => ipcRenderer.invoke('exists-assets', filePath),
+    exists: (filePath) => ipcRenderer.invoke('exists', filePath),
+    existsAppdata: (filePath) => ipcRenderer.invoke('exists-appdata', filePath),
+    devTools: () => ipcRenderer.send('devTools'),
+    isDev: isDev,
+    welcomeDone: () => ipcRenderer.send('welcome:done'),
+    menu: { maximizeApp: () => ipcRenderer.send('menu:maximize-app'),
+        minimizeApp: () => ipcRenderer.send('menu:minimize-app'),
+        closeApp: () => ipcRenderer.send('menu:close-app'),
+        isMaximized: () => ipcRenderer.invoke('menu:is-maximized'),
+        dragWindow: (delta) => ipcRenderer.send('drag-window', delta) },
+    json: { load: (filePath) => ipcRenderer.invoke('json:load', filePath),
+        save: (filePath, data) => ipcRenderer.invoke('json:save', { filePath, data }) },
+    config: { getConfig: () => ipcRenderer.invoke('config:get'),
+        updateConfig: (key, value) => ipcRenderer.send('config:update', { key, value }) },
+    notes: {
+        rename: (oldName, newName) => ipcRenderer.invoke('notes:rename', oldName, newName)
+    },
+    themes: {
+        list: () => ipcRenderer.invoke('themes:list'),
+        get: (name) => ipcRenderer.invoke('themes:get', name),
+        getCurrent: () => ipcRenderer.invoke('themes:get-current')
+    },
+    checkAssetsStatus: () => ipcRenderer.invoke('assets-check-status'),
+    onAssetsProgress: (callback) => {
+        const listener = (_, data) => callback(data);
+        ipcRenderer.on('assets-progress', listener);
+        return () => ipcRenderer.removeListener('assets-progress', listener);
+    },
+    onAssetsReady: (callback) => {
+        ipcRenderer.on('assets-ready', () => callback());
+    },
+    onAssetsError: (callback) => {
+        ipcRenderer.on('assets-error', (event, errorMessage) => callback(errorMessage));
+    },
+    changelog: {
+        check: () => ipcRenderer.invoke('changelog:check'),
+        markSeen: () => ipcRenderer.invoke('changelog:mark-seen'),
+        get: () => ipcRenderer.invoke('changelog:get')
+    },
+    updates: {
+        checkUpdates: () => ipcRenderer.invoke('updates:check-update')
+    },
+    onUpdateStatus: (callback) => ipcRenderer.on('update-status', (event, msg) => callback(msg)),
+    onUpdateProgress: (callback) => ipcRenderer.on('update-progress', (event, percent) => callback(percent)),
+    onUpdateReady: (callback) => ipcRenderer.on('update-ready-to-install', callback),
+    restartAndInstall: () => ipcRenderer.send('update:restart'),
+    checkUpdateStatus: () => ipcRenderer.invoke('update:check-status'),
+    syncAssets: () => ipcRenderer.invoke('sync-assets')
+});
+contextBridge.exposeInMainWorld('api', {
+    load: (path) => ipcRenderer.invoke('load', path),
+    notes: {
+        create: (name) => ipcRenderer.invoke('notes:create', name),
+        delete: (name) => ipcRenderer.invoke('notes:delete', name),
+        save: (name, content) => ipcRenderer.invoke('notes:save', name, content),
+        count: () => ipcRenderer.invoke('notes:count'),
+        saveOrder: (content) => ipcRenderer.invoke('notes:save-order', content),
+        selectAndImage: () => ipcRenderer.invoke('notes:select-add-image')
+    },
+    games: { addGame: (gameData) => ipcRenderer.invoke('games:add', gameData),
+        ensureCover: (data) => ipcRenderer.invoke('games:ensure-cover', data),
+        finishedCount: () => ipcRenderer.invoke('games:finished-count'),
+        achieCount: () => ipcRenderer.invoke('games:achie-count'),
+        getSteamData: (appid) => ipcRenderer.invoke('games:get-steam-data', appid),
+        addGame: (gameData) => ipcRenderer.invoke('games:add', gameData) },
+    openLink: (url) => ipcRenderer.invoke('open-external-link', url),
+    getAppVersion: () => ipcRenderer.invoke('app-version'),
+    fortnite: { getTrailers: () => ipcRenderer.invoke('fortnite:fetch-trailers'),
+        getSeasons: () => ipcRenderer.invoke('fortnite:fetch-seasons'),
+        listTrailers: () => ipcRenderer.invoke('fortnite:list-trailers') },
+    assetsConfig: {
+        get: () => ipcRenderer.invoke('assets-config:get'),
+        update: (key, value) => ipcRenderer.send('assets-config:update', key, value)
+    },
+    addGameToGist: (gameData) => ipcRenderer.invoke('add-game-to-gist', gameData),
+    restartApp: () => ipcRenderer.send('update:restart')
+});
+contextBridge.exposeInMainWorld('info', {
+    getUserData: () => ipcRenderer.invoke('info:user-data'),
+    getDocuments: () => ipcRenderer.invoke('info:documents')
+});
+//# sourceMappingURL=preload.js.map
